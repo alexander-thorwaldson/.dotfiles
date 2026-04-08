90b1332c49ec9b37fe79d1dc8ab693597d6b10e3
Alex Thorwaldson <alex.thorwaldson@seerist.com>
2026-04-07 18:02:41 -0700

    feat: update nvim deps + set init fish msg

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,8 @@
+function fish_greeting
+    echo "
+Wake up, "(git config user.name)"..."
+end
+
 # SSH agent setup - reuse existing or start new
 if test -z "$SSH_AUTH_SOCK"
     set -l SSH_AGENT_FILE ~/.ssh/ssh-agent-info
